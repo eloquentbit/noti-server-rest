5731a1533c61653885de1c28cf5feb413865b550
Luca Peduto <luca@eloquentbit.com>
2017-08-06 14:56:44 +0200

    Add note model

---
+++ src/models/note.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const NoteSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  public: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const Note = mongoose.model('Note', NoteSchema)
+
+module.exports = { Note }
