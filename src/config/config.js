3a2e71ed1373711b239b4b6623f0c93846586819
Luca Peduto <luca@eloquentbit.com>
2017-08-13 15:28:07 +0200

    Add logging configuration

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,18 @@
 import yenv from 'yenv'
+import winston from 'winston'
 
+// Read env file
 const env = yenv()
 
-export default env
+// Configure logger module
+const tsFormat = () => new Date().toLocaleTimeString()
+const logger = new winston.Logger({
+  transports: [
+    new winston.transports.Console({
+      timestamp: tsFormat,
+      colorize: true
+    })
+  ]
+})
+
+export { env, logger }
