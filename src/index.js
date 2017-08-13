5dcd16c5a2a3d6aae489a5e5260a8c8f040036cd
Luca Peduto <luca@eloquentbit.com>
2017-08-13 15:18:47 +0200

    Configure morgan package

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import env from './config/config'
 
 import express from 'express'
 import bodyParser from 'body-parser'
+import morgan from 'morgan'
 
 import db from './db/mongoose'
 
 const PORT = env.PORT || 8080
 
 const app = express()
+
+app.use(
+  morgan('dev', {
+    skip: () => app.get('env') === 'test'
+  })
+)
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
