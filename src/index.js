e7ebf5a970ef00a675b8b622edefc2ac837c107b
Luca Peduto <luca@eloquentbit.com>
2017-08-13 09:31:08 +0200

    Initialize mongoose connection

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import env from './config/config'
 import express from 'express'
 import bodyParser from 'body-parser'
 
+import mongoose from './db/mongoose'
+
 const PORT = env.PORT || 8080
 
 const app = express()
