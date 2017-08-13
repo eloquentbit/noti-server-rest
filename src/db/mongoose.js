4a2b9c7831d31cb7d79f107895ea05f1eefa876a
Luca Peduto <luca@eloquentbit.com>
2017-08-13 15:28:33 +0200

    Use winston to log messages

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,13 +1,13 @@
-import env from '../config/config'
+import { env, logger } from '../config/config'
 import mongoose from 'mongoose'
 
 mongoose.Promise = global.Promise
 mongoose.connect(env.MONGODB_URI, { useMongoClient: true }).then(
   () => {
-    console.log(`Connected to database at ${env.MONGODB_URI}`)
+    logger.info(`Connected to database at ${env.MONGODB_URI}`)
   },
   err => {
-    console.error(err)
+    logger.error(err)
   }
 )
 
