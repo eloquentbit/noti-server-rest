dc81edac411929742af4583ea922f9cb00399218
Luca Peduto <luca@eloquentbit.com>
2017-08-20 10:49:57 +0200

    Add tests for POST /notes

--- src/tests/server.test.js
+++ src/tests/server.test.js
@@ -1,13 +1,18 @@
 /* eslint-env mocha */
 
-const expect = require('expect')
-const request = require('supertest')
-const winston = require('winston')
+import expect from 'expect'
+import request from 'supertest'
+import winston from 'winston'
 
-const { app } = require('./../index')
-const { logger } = require('./../config/config')
+import { app } from './../index'
+import Note from './../models/note'
+import { logger } from './../config/config'
 
 logger.remove(winston.transports.Console)
+
+beforeEach(done => {
+  Note.remove({}).then(() => done())
+})
 
 describe('GET /', () => {
   it('should respond with message `Server Ready`', done => {
@@ -18,5 +23,32 @@ describe('GET /', () => {
         expect(res.body.message).toBe('Server Ready')
       })
       .end(done)
+  })
+})
+
+describe('POST /notes', () => {
+  it('should create a new note', done => {
+    const content = '# Note title\nFirst note'
+
+    request(app)
+      .post('/notes')
+      .send({ content })
+      .expect(200)
+      .expect(res => {
+        expect(res.body.content).toBe(content)
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err)
+        }
+
+        Note.find({ content })
+          .then(notes => {
+            expect(notes.length).toBe(1)
+            expect(notes[0].content).toBe(content)
+            done()
+          })
+          .catch(e => done(e))
+      })
   })
 })
