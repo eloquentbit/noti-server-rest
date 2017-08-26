473bd2220b411e3a87f7f37eaf76549912258870
Luca Peduto <luca@eloquentbit.com>
2017-08-26 08:55:42 +0200

    Add test data for user model and create populateUsers method

--- src/tests/seed/seed.js
+++ src/tests/seed/seed.js
@@ -1,9 +1,15 @@
+import { env } from './../../config/config'
 import { ObjectID } from 'mongodb'
+import jwt from 'jsonwebtoken'
 
 import Note from './../../models/note'
+import { User } from './../../models/user'
 
 const noteOneId = new ObjectID()
 const noteTwoId = new ObjectID()
+
+const userOneId = new ObjectID()
+const userTwoId = new ObjectID()
 
 const notes = [
   {
@@ -16,6 +22,35 @@ const notes = [
   }
 ]
 
+const users = [
+  {
+    _id: userOneId,
+    email: 'john@doe.com',
+    password: 'userOnePass',
+    tokens: [
+      {
+        access: 'auth',
+        token: jwt
+          .sign({ _id: userOneId, access: 'auth' }, env.JWT_SECRET)
+          .toString()
+      }
+    ]
+  },
+  {
+    _id: userTwoId,
+    email: 'jen@example.com',
+    password: 'userTwoPass',
+    tokens: [
+      {
+        access: 'auth',
+        token: jwt
+          .sign({ _id: userOneId, access: 'auth' }, env.JWT_SECRET)
+          .toString()
+      }
+    ]
+  }
+]
+
 const populateNotes = done => {
   Note.remove({})
     .then(() => {
@@ -24,4 +59,12 @@ const populateNotes = done => {
     .then(() => done())
 }
 
-export { notes, populateNotes }
+const populateUsers = done => {
+  User.remove({})
+    .then(() => {
+      return User.insertMany(users)
+    })
+    .then(() => done())
+}
+
+export { notes, users, populateNotes, populateUsers }
